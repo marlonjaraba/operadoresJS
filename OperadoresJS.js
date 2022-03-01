d0d5084670d06c1cd6f0e2bd415ccda2ba575a6e
marlonjaraba <marlondjg@outlook.com>
2022-02-28 21:49:23 -0500

    subiendo un archivo js

---
+++ OperadoresJS.js
@@ -0,0 +1,44 @@
+let a = 3, b = 6;
+let result = a + b;
+console.log("el resultado es: "+result);
+
+// operadores  aritmeticos
+result = b - a;
+console.log("el resultado es: "+result);
+
+result = a * b;
+console.log("el resultado es: " + result);
+
+result = a / b;
+console.log("el resultado es: "+result);
+
+result = a % b;
+console.log("el resultado es: "+ result);
+
+result = a ** b;
+console.log("el resultado es: "+result);
+
+// operadores de incremento y decremento 
+
+let x = 4, y = 6;
+let p = x + y;
+
+// incremento 
+// pre-incremento (el operador ++ antes de la variable)
+
+p = ++x;
+console.log(x);
+console.log(p);
+
+// post-incremento (el opreador ++ despues de la variable)
+
+p = y++;
+console.log(y);
+console.log(p);
+
+// decremento
+// pre-decremento (el operador -- antes de la variable)
+
+p = x--;
+console.log(x)
+console.log(p)
